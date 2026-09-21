0126ecb1f951c9e64239149a658780c75057ed67
Benjamin Mulligan <benjamin.mulligan@virginmoney.com>
2026-09-21 17:51:34 +0100

    add navigation bar top and bottom toggle

--- src/settings/pages/git.rs
+++ src/settings/pages/git.rs
@@ -236,7 +236,8 @@ pub fn handle_enter(cursor: usize, ss: &mut SettingsState) {
                     "clock_show_seconds": false,
                     "border_style": "Rounded",
                     "status_bar_enabled": false,
-                    "new_window_keeps_tuix_open": false
+                    "new_window_keeps_tuix_open": false,
+                    "navbar_position": "Top"
                 },
                 "display": {
                     "fullscreen": false
